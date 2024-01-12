53e208b3734f48660ab3b71fc58710127379ac10
adhrox97 <adcom3@hotmail.com>
2024-01-12 18:51:28 -0500

    Modificacion de PlannerViewModel

--- app/src/main/java/com/adhrox/gymplanner/ui/planner/PlannerViewModel.kt
+++ app/src/main/java/com/adhrox/gymplanner/ui/planner/PlannerViewModel.kt
@@ -1,9 +1,19 @@
 package com.adhrox.gymplanner.ui.planner
 
 import androidx.lifecycle.ViewModel
+import com.adhrox.gymplanner.data.providers.DaysProvider
+import com.adhrox.gymplanner.domain.model.DayInfo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class PlannerViewModel @Inject constructor(): ViewModel() {
+class PlannerViewModel @Inject constructor(daysProvider: DaysProvider): ViewModel() {
+    private  var _days = MutableStateFlow<List<DayInfo>>(emptyList())
+    val days:StateFlow<List<DayInfo>> = _days
+
+    init {
+        _days.value = daysProvider.getDays()
+    }
 }
